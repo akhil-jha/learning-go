09cd81af7a74e23bd899ed6ef0457afa98b4deb2
Akhil Jha <akjha@redhat.com>
2024-01-04 23:09:23 +0530

    Function calls in go

--- Functions/main.go
+++ Functions/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 // func testFunction(firstNumber, secondNumebr int, test string) (int, int, string) {
@@ -32,18 +31,31 @@ import (
 // }
 
 // call by refernce
-func swapNumbers(firstNumber, secondNumber *int) {
-	var tempNumber int
-	tempNumber = *firstNumber
-	*firstNumber = *secondNumber
-	*secondNumber = tempNumber
+// func swapNumbers(firstNumber, secondNumber *int) {
+// 	var tempNumber int
+// 	tempNumber = *firstNumber
+// 	*firstNumber = *secondNumber
+// 	*secondNumber = tempNumber
+// }
+
+// func main() {
+// 	firstNumber := 1
+// 	secondNumber := 2
+// 	fmt.Printf("Before swapping\nFirst number: %v\nSecond number: %v\n", firstNumber, secondNumber)
+// 	fmt.Println(strings.Repeat("-", 10))
+// 	swapNumbers(&firstNumber, &secondNumber)
+// 	fmt.Printf("After swapping\nFirst number: %v\nSecond number: %v\n", firstNumber, secondNumber)
+// }
+
+// Sum of entire array
+func sumOfNumbers(numbers ...int) int {
+	sum := 0
+	for _, value := range numbers {
+		sum = sum + value
+	}
+	return sum
 }
 
 func main() {
-	firstNumber := 1
-	secondNumber := 2
-	fmt.Printf("Before swapping\nFirst number: %v\nSecond number: %v\n", firstNumber, secondNumber)
-	fmt.Println(strings.Repeat("-", 10))
-	swapNumbers(&firstNumber, &secondNumber)
-	fmt.Printf("After swapping\nFirst number: %v\nSecond number: %v\n", firstNumber, secondNumber)
+	fmt.Printf("Sum of the numbers is: %v\n", sumOfNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 }
