6465a8bf5e5955ccd3e4fa68ab221832ab1928a4
Akhil Jha <38880644+akhil-jha@users.noreply.github.com>
2025-05-05 00:03:48 +0530

    update

--- PrintingFormattingStrings/main.go
+++ PrintingFormattingStrings/main.go
@@ -5,22 +5,22 @@ import (
 )
 
 func main() {
-	var tempA string = "I am a string"
+	var tempA string = "String"
 	var numA = 100
 	numB := 11
 	var emptyVar string
-	emptyVar = "foobar"
+	emptyVar = "emptyVar"
 
 	fmt.Println("These are the variables I declared above:", tempA, numA, numB, "\nI can do this as well: ", emptyVar)
 
 	fmt.Print("Hellonn") // Doesn't add new line on output
-	fmt.Println("Hellonn")
+	fmt.Println("Hellonn\n\n\n")
 
-	fmt.Printf("I can do this - %v - as well - %v - here which is cool - %v - too I guess\n", tempA, numA, numB)
-	fmt.Printf("I can do this - %q - as well - %q - here which is cool - %q - too I guess\n", tempA, numA, numB) // %q only works for strings
-	fmt.Printf("This is a float formatted string %0.3f okay?", 132.312334)
+	fmt.Printf("percentage V values -> %v - %v - %v", tempA, numA, numB)
+	fmt.Printf("\n\nMix value -> %q - %q - %q -> q only works for strings", tempA, numA, numB,) // %q only works for strings
+	fmt.Printf("\n\nFloat string %0.3f: ", 132.312334)
 
 	// save formatted string
-	var newString = fmt.Sprintf("I can do this - %v - as well - %v - here which is cool - %v - too I guess\n", tempA, numA, numB)
+	var newString = fmt.Sprintf("\n\n %v %v %v", tempA, numA, numB)
 	fmt.Println(newString)
 }
